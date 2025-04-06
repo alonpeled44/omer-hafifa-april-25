1f21ed9d374df36bebd22192e77fe5b7c3c91dab
omerMai21 <maimonomer7@gmail.com>
2025-04-06 15:46:49 +0300

    -Add css file inside login component -create css file for login and for header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,9 @@
+import Login from "./login";
+
 export default function Home() {
   return (
     <>
-      <h1>Hi</h1>
-      <p>first project</p>
+      <Login/>
     </>
   );
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,40 +1,41 @@
 import React from "react";
+import styles from "../styles/pages/login.module.css";
 
 function Login() {
   return (
-    <>
-      <main>
-        <form id="form">
-          <h1>Login</h1>
+    <main>
+      <form id="form">
+        <h1>Login</h1>
 
-          <section class="inputs">
-            <div>
-              <input
-                id="username"
-                type="text"
-                placeholder="username"
-                autocomplete="username"
-              />
-              <input
-                id="password"
-                type="password"
-                placeholder="password"
-                autocomplete="current-password"
-              />
-            </div>
-            <div id="error-message"></div>
-          </section>
+        <section className={styles.inputs}>
+          <div>
+            <input
+              id="username"
+              type="text"
+              placeholder="username"
+              autocomplete="username"
+            />
+            <input
+              id="password"
+              type="password"
+              placeholder="password"
+              autocomplete="current-password"
+            />
+          </div>
+          <div id="error-message"></div>
+        </section>
 
-          <section class="login-buttons-container">
-            <button class="login-button" onclick="formValidation(event)">
-              login
-            </button>
-            <button class="guest-button" onclick="welcomeGuest()">
-              join as guest
-            </button>
-          </section>
-        </form>
-      </main>
-    </>
+        <section className="login-buttons-container">
+          <button className="login-button" onclick="formValidation(event)">
+            login
+          </button>
+          <button className="guest-button" onclick="welcomeGuest()">
+            join as guest
+          </button>
+        </section>
+      </form>
+    </main>
   );
 }
+
+export default Login;
