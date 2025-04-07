ed34bafa610c4ab2c59ca9155e638d6d387c26ac
omerMai21 <maimonomer7@gmail.com>
2025-04-07 14:18:18 +0300

    Importing pokemon icon image to page header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import pokemonIcon from "../photos/pokemon-photo.png";
 export default function Header() {
   return (
     <>
       <header>
         <div className="logo-header">
-          <img src="/photos/pokemon-photo.png" alt="pokemon-photo" />
+          <img src={pokemonIcon.src} />
           <p id="header-text">pokemon</p>
         </div>
 
