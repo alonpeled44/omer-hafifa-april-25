167305fcdfdc6dfb7cc1a862befdac99667c18d9
omerMai21 <maimonomer7@gmail.com>
2025-04-08 15:20:37 +0000

    Add event handlers for blocking invalid characters, checking user data exist in the usersData array and alerting welcome guest when button for guests clicked

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {use, useState} from "react";
 import styles from "../styles/pages/login.module.css";
 
 function Login() {
@@ -9,49 +9,85 @@ function Login() {
     ];
 
     const [user, setUser] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleSubmit = (event) => {
       event.preventDefault();
 
-    }
+      if(!user._username || !user._password)
+      {
+        setErrorMessage('Username or password are empty');
+        return;
+      }
 
-    const handleChange = ({target}) => {
-      const {name, value} = target;
-       setUser((prevUser) => ({
-        ...prevUser,
-        [name]: value
-       }));
-    }
+      const foundUser = usersData.find(currentUser => {
+        currentUser._username===user._username && currentUser._password===user._password;
+      });
+
+      if(foundUser){
+        alert(`Welcome ${user._username}`);
+        setErrorMessage('');
+      }
+      else{
+        setErrorMessage('Username or password incorrect');
+      }
+    };
+
+    const handleGuestClick = () => {
+       alert('Welcome guest');
+    };
+
+    const handleInputChange = (event) => {
+      let {name, value} = event.target;
+      setTimeout(() => {
+        const regex = /[^A-Za-z0-9]/g;
+        value = value.replace(regex, "");
+        console.log("after replace: "+value);
+        setUser((prev) => ({
+          ...prev,
+          [name]: value
+        }));
+        console.log(user);
+      }, 0);
+    };
 
 
   return (
     <main>
-      <form id="form">
+      <form id="form" onSubmit={handleSubmit}>
         <h1>Login</h1>
 
         <section className={styles.inputs}>
           <div>
             <input
               id="username"
+              name="_username"
               type="text"
+              value={user._username || ''}
+              onChange={handleInputChange}
               placeholder="username"
               autocomplete="username"
             />
             <input
               id="password"
+              name="_password"
               type="password"
+              value={user._password || ''}
+              onChange={handleInputChange}
               placeholder="password"
               autocomplete="current-password"
             />
           </div>
-          <div id="error-message"></div>
+          <div id="error-message" style={{ color: "red", fontSize: 25 }}>
+            {errorMessage}
+          </div>
         </section>
 
         <section className={styles["login-buttons-container"]}>
-          <button className={styles["login-button"]} onclick="formValidation(event)">
+          <button className={styles["login-button"]} type="submit">
             login
           </button>
-          <button className={styles["guest-button"]} onclick="welcomeGuest()">
+          <button className={styles["guest-button"]} type="button" onClick={handleGuestClick}>
             join as guest
           </button>
         </section>
