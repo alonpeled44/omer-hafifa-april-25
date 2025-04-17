f88ab1b3a6556eb627c67ee57748ed1d86b6b03a
omerMai21 <maimonomer7@gmail.com>
2025-04-17 14:08:15 +0300

    add useEffect to remove username from localStorage and redirect back to login page(need to work more on this to work!!!)

--- src/pages/userMenu.jsx
+++ src/pages/userMenu.jsx
@@ -1,6 +1,13 @@
-
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 function userMenu(props) {
+  const router = useRouter();
+  useEffect(()=> {
+    localStorage.removeItem(props.key);
+    router.push('/login.jsx');
+  }, [])
+
   return (
     <div>
         <p>Hello {props.username}</p>
