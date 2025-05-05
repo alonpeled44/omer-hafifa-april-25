879159ebbfdf174a62ad6c00b1c5f48a2fb2c65e
omerMai21 <maimonomer7@gmail.com>
2025-05-05 14:16:32 +0300

    Adding p elements which contains username Adding button for log out without functionality

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,31 @@
 import { useScreenWidth } from "../libs/screenContext";
 import pokemonIcon from "../images/pokemon-photo.png";
 import styles from "../styles/components/header.module.css";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
   const { screenWidth } = useScreenWidth();
+  const [username,setUsername]=useState('');
+  const pathname = usePathname();
+
+  useEffect(()=> {
+    const storedUsername = localStorage.getItem("username");
+    setUsername(storedUsername || '');
+  }, [pathname]);
 
   return (
     <header className={styles.header}>
       <div className={styles["logo-header"]}>
         <img src={pokemonIcon.src} />
         <p className={styles["header-text"]}>pokemon</p>
-        <p></p>
-        <button></button>
+        {pathname==='/' && 
+        (
+          <div className={styles["header-menu"]}>
+            <p>{username}</p>
+            <button>log out</button>
+          </div>
+        )}
       </div>
 
       {screenWidth > 1200 && (
