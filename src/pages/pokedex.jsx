c1989c131c95d426c417a044cae3f49f393cbda6
omerMai21 <maimonomer7@gmail.com>
2025-05-07 15:57:31 +0300

    -Add div to contain div of card container and div of customize row -Add div for customize cards inside card container -add input element for serach in customize container

--- src/pages/pokedex.jsx
+++ src/pages/pokedex.jsx
@@ -3,20 +3,25 @@ import { pokemonCardsArray } from "../components/pokemonCards";
 import styles from "../styles/pages/pokedex.module.css";
 export default function pokedex() {
   return (
-    <div className={styles["cards-container"]}>
-        {pokemonCardsArray.map((card)=> (
-            <Card
-              id={card.id}
-              name={card.name}
-              type={card.type}
-              height={card.height} 
-              weight={card.weight} 
-              frontViewImageUrl={card.frontViewImageUrl} 
-              backViewImageUrl={card.backViewImageUrl} 
-              frontShinyViewImageUrl={card.frontShinyViewImageUrl} 
-              backShinyViewImageUrl={card.backShinyViewImageUrl} 
-            />
+    <div className={styles["pokedex-page-container"]}>
+      <div className={styles["custom-cards-container"]}>
+        <input type="text" placeholder="Search..." />
+      </div>
+      <div className={styles["cards-container"]}>
+        {pokemonCardsArray.map((card) => (
+          <Card
+            id={card.id}
+            name={card.name}
+            type={card.type}
+            height={card.height}
+            weight={card.weight}
+            frontViewImageUrl={card.frontViewImageUrl}
+            backViewImageUrl={card.backViewImageUrl}
+            frontShinyViewImageUrl={card.frontShinyViewImageUrl}
+            backShinyViewImageUrl={card.backShinyViewImageUrl}
+          />
         ))}
+      </div>
     </div>
   );
 }
