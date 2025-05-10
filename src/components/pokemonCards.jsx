6d2ea16f643afec8baf1ea2136a7f6179ec77aca
omerMai21 <maimonomer7@gmail.com>
2025-05-10 14:02:01 +0000

    -Increase height of grid container of cards -Increase gap between card info -Increase margin top between card data and card image -Add linear gardient as background for each card

--- src/components/pokemonCards.jsx
+++ src/components/pokemonCards.jsx
@@ -111,5 +111,93 @@ export const pokemonCardsArray = [
         backViewImageUrl: '',
         frontShinyViewImageUrl: '',
         backShinyViewImageUrl: ''
+    },
+    {
+        id: 1,
+        name: 'pikachu',
+        type: typesArray[0],
+        height: 145,
+        weight: 45,
+        frontViewImageUrl: pokemonImage.src,
+        backViewImageUrl: '',
+        frontShinyViewImageUrl: '',
+        backShinyViewImageUrl: ''
+    },
+    {
+        id: 1,
+        name: 'pikachu',
+        type: typesArray[0],
+        height: 145,
+        weight: 45,
+        frontViewImageUrl: pokemonImage.src,
+        backViewImageUrl: '',
+        frontShinyViewImageUrl: '',
+        backShinyViewImageUrl: ''
+    },
+    {
+        id: 1,
+        name: 'pikachu',
+        type: typesArray[0],
+        height: 145,
+        weight: 45,
+        frontViewImageUrl: pokemonImage.src,
+        backViewImageUrl: '',
+        frontShinyViewImageUrl: '',
+        backShinyViewImageUrl: ''
+    },
+    {
+        id: 1,
+        name: 'pikachu',
+        type: typesArray[0],
+        height: 145,
+        weight: 45,
+        frontViewImageUrl: pokemonImage.src,
+        backViewImageUrl: '',
+        frontShinyViewImageUrl: '',
+        backShinyViewImageUrl: ''
+    },
+    {
+        id: 1,
+        name: 'pikachu',
+        type: typesArray[0],
+        height: 145,
+        weight: 45,
+        frontViewImageUrl: pokemonImage.src,
+        backViewImageUrl: '',
+        frontShinyViewImageUrl: '',
+        backShinyViewImageUrl: ''
+    },
+    {
+        id: 1,
+        name: 'pikachu',
+        type: typesArray[0],
+        height: 145,
+        weight: 45,
+        frontViewImageUrl: pokemonImage.src,
+        backViewImageUrl: '',
+        frontShinyViewImageUrl: '',
+        backShinyViewImageUrl: ''
+    },
+    {
+        id: 1,
+        name: 'pikachu',
+        type: typesArray[0],
+        height: 145,
+        weight: 45,
+        frontViewImageUrl: pokemonImage.src,
+        backViewImageUrl: '',
+        frontShinyViewImageUrl: '',
+        backShinyViewImageUrl: ''
+    },
+    {
+        id: 1,
+        name: 'pikachu',
+        type: typesArray[0],
+        height: 145,
+        weight: 45,
+        frontViewImageUrl: pokemonImage.src,
+        backViewImageUrl: '',
+        frontShinyViewImageUrl: '',
+        backShinyViewImageUrl: ''
     }
 ];
