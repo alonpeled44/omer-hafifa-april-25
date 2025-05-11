cd0d369263574d9e8d63eceee1d77762fd93a41c
omerMai21 <maimonomer7@gmail.com>
2025-05-11 14:50:45 +0300

    -Add select boxes of filter by and sort by to pokedex.jsx -Add space between filter and sort container to search field -Add space between filter by and sort by

--- src/pages/pokedex.jsx
+++ src/pages/pokedex.jsx
@@ -6,6 +6,25 @@ export default function pokedex() {
     <div className={styles["pokedex-page-container"]}>
       <div className={styles["custom-cards-container"]}>
         <input type="text" placeholder="Search..." />
+        <section className={styles["filter-and-sort"]}>
+          <select className={styles["filter-by"]}>
+            <option value="" disabled selected>
+              filter by
+            </option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+
+          <select className={styles["sort-by"]}>
+            <option value="" disabled selected>
+              sort by
+            </option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </section>
       </div>
       <div className={styles["cards-container"]}>
         {pokemonCardsArray.map((card) => (
