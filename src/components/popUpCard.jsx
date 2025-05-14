5ea5993147f620bda570c66fc10b049f50210cac
omerMai21 <maimonomer7@gmail.com>
2025-05-14 15:49:53 +0300

    -change name of pop up card component -create empty tree of the pop up card component

---
+++ src/components/popUpCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function PopUpCard() {
+  return (
+    <div>
+        <section>
+
+        </section>
+
+        <section>
+
+        </section>
+
+        <section>
+            <p></p>
+            <p></p>
+            <p></p>
+        </section>
+    </div>
+  )
+}
