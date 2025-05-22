328b656007aa192c3a3bd94cb1c32a14f1ebe731
omerMai21 <maimonomer7@gmail.com>
2025-05-22 11:22:18 +0300

    Organize pokemonCards.jsx code

--- src/components/pokemonCards.jsx
+++ src/components/pokemonCards.jsx
@@ -2,6 +2,7 @@ import pokemonFrontImage from "../images/pikachu-front-image.png";
 import pokemonBackImage from "../images/pikachu-back-image.png";
 import pokemonFrontShinyImage from "../images/pikachu-front-shiny-image.png";
 import pokemonBackShinyImage from "../images/pikachu-back-shiny-image.png";
+
 const typesArray = ['fire', 'water', 'wind', 'land'];
 
 export const pokemonCardsArray = [
