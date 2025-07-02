c0426798735b79901612cef31d87f7e5d3595fcd
omerMai21 <maimonomer7@gmail.com>
2025-07-02 16:01:15 +0300

    in single select the option is object with value of string (maybe change it later)

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -34,7 +34,7 @@ export default function Select({
                   }
                 }
                 else {
-                  setSelectedOptions(option);
+                  setSelectedOptions(option.value);
                 }
               }}
             ></button>
